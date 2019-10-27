16cc97a6c874e3a9e0028f75de9b6c34ffdda01f
valen214 <xvalen214x@gmail.com>
2019-10-28 04:31:43 +0800

    0431 28 Oct 2019: confused

---
+++ src/event_handlers.js
@@ -0,0 +1,21 @@
+
+window.addEventListener("load", function(e){
+
+const side_nav_sign_in = document.getElementById("side_nav_sign_in")
+side_nav_sign_in.addEventListener("click", myclip.signIn);
+
+const create_clip_button = document.getElementById("create_clip_button");
+create_clip_button.addEventListener("click", () => {
+  create_clip_options
+});
+
+const create_clip_options_text =
+    document.getElementById("create_clip_options_text");
+const create_clip_options_file =
+    document.getElementById("create_clip_options_file");
+const create_clip_options_image =
+    document.getElementById("create_clip_options_image");
+
+
+
+});
---
+++ src/myclip.js
@@ -0,0 +1,17 @@
+
+
+(function(window){
+
+var myclip = window["myclip"] = {
+  signIn(){
+
+  },
+  addClipItem(data){
+
+  },
+  removeClipItem(id){
+
+  }
+};
+
+})(window);
