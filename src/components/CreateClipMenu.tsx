c95bb341c532cf8eebede953a7ffdc1673f34fa8
Valen Cheung <xvalen214x@gmail.com>
2019-11-02 14:36:00 +0800

    1435 02 Nov 2019

--- src/components/CreateClipMenu.tsx
+++ src/components/CreateClipMenu.tsx
@@ -3,9 +3,13 @@ import React, { useState, useEffect } from "react";
 
 import { makeStyles, Theme, createStyles }
     from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Fab from '@material-ui/core/Fab';
 import Fade from "@material-ui/core/Fade";
+import Grid from "@material-ui/core/Grid";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Zoom from "@material-ui/core/Zoom";
@@ -31,6 +35,23 @@ const CreateCilpMenu = ({showButton, className}: any) => {
   }
 
   return <React.Fragment>
+    <Box display="inline" style={{
+        position: "absolute",
+        top: "100px",
+        background: "#eee",
+      }}>
+      <GridList cols={2}
+          cellHeight={100}
+          style={{ width: "200px" }}>
+        {["text", "image", "file", "folder"].map(text => (
+          <GridListTile>
+            <Button fullWidth style={{ height: "100%" }}>
+              {text}
+            </Button>
+          </GridListTile>
+        ))}
+      </GridList>
+    </Box>
     <Fade in={showButton}>
       <Fab className={className}
           onClick={handleClick}>
@@ -40,12 +61,17 @@ const CreateCilpMenu = ({showButton, className}: any) => {
     <Menu
         keepMounted
         anchorEl={anchorEl}
+        getContentAnchorEl={null}
         open={Boolean(anchorEl) && showButton}
         onClose={handleClose}
-        TransitionComponent={Transition}>
-      <MenuItem>
-        <Button>Hello!</Button>
-      </MenuItem>
+        TransitionComponent={Transition}
+        anchorOrigin={{
+          vertical: 'center', horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: "center", horizontal: "center"
+        }}>
+        <div>HELLO </div>
     </Menu>
   </React.Fragment>;
 };
