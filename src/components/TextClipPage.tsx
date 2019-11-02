30e2ca4ed739d0bf1c31fe16dd16d7cf754d6823
valen214 <xvalen214x@gmail.com>
2019-11-03 04:07:53 +0800

    0407 03 Nov 2019

--- src/components/TextClipPage.tsx
+++ src/components/TextClipPage.tsx
@@ -1,8 +1,10 @@
 
 import React, { useState, useEffect } from "react";
 
+import { sizing } from '@material-ui/system';
 import { makeStyles, Theme, createStyles }
     from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box';
 import Dialog from "@material-ui/core/Dialog";
 import Slide from "@material-ui/core/Slide";
 import TextAreaAutosize from "@material-ui/core/TextAreaAutosize";
@@ -23,19 +25,23 @@ const TextClipPage = ({ open, onClose, onDone }: any) => {
   return <Dialog fullScreen
       open={open}
       TransitionComponent={Transition}>
-    <TopNav
-        position="static"
-        mode={TopNavMode.input}
-        onClose={onClose}
-        onChange={setTitle}
-        onDone={() => onDone(title, value)}
-        doneIcon={<DoneIcon />}
-        placeholder="Title (Optional)" />
-    <TextAreaAutosize
-        onChange={(e: React.FormEvent<HTMLInputElement>) => {
-          setValue(e.currentTarget.value);
-        }}
-        style={{ height: "500px", width: "500px" }} />
+    <Box display="flex" flexDirection="column" width="100%" height="100%">
+      <TopNav
+          position="static"
+          mode={TopNavMode.input}
+          onClose={onClose}
+          onChange={setTitle}
+          onDone={() => onDone(title, value)}
+          doneIcon={<DoneIcon />}
+          placeholder="Title (Optional)" />
+      <Box width="100%" height="100%">
+        <TextAreaAutosize
+            onChange={(e: React.FormEvent<HTMLInputElement>) => {
+              setValue(e.currentTarget.value);
+            }}
+            style={{ resize: "none", }} />
+      </Box>
+    </Box>
   </Dialog>;
 };
 
